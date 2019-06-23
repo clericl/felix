6fc74418b5b50c3074652e66a0d2730a45b91bea
Eric Liang <eliang58@gmail.com>
2019-06-23 19:22:48 -0400

    complete timeline photo styling

--- frontend/components/main/posts/post_item_photo_index.jsx
+++ frontend/components/main/posts/post_item_photo_index.jsx
@@ -4,14 +4,76 @@ import Grid from '@material-ui/core/Grid';
 class PostItemPhotoIndex extends React.Component {
     constructor(props) {
         super(props);
+
+        this.renderIndex = this.renderIndex.bind(this);
+    }
+
+    renderIndex() {
+        const numPhotos = this.props.photos.length;
+        switch (numPhotos) {
+            case 0:
+                return null;
+            case 1:
+                return (
+                    <img className="post-item-photo-free" src={this.props.photos[0]} />
+                )
+            case 2:
+                return (
+                    <div className="post-item-photo-index">
+                        <img className="post-item-photo-half" src={this.props.photos[0]} />
+                        <img className="post-item-photo-half" src={this.props.photos[1]} />
+                    </div>
+                )
+            case 3:
+                return (
+                    <div className="post-item-photo-index">
+                        <img className="post-item-photo-23" src={this.props.photos[0]} />
+                        <img className="post-item-photo-13" src={this.props.photos[1]} />
+                        <img className="post-item-photo-13" src={this.props.photos[2]} />
+                    </div>
+                )
+            case 4:
+                return (
+                    <div className="post-item-photo-index">
+                        <img className="post-item-photo-quarter" src={this.props.photos[0]} />
+                        <img className="post-item-photo-quarter" src={this.props.photos[1]} />
+                        <img className="post-item-photo-quarter" src={this.props.photos[2]} />
+                        <img className="post-item-photo-quarter" src={this.props.photos[3]} />
+                    </div>
+                )
+            case 5:
+                return (
+                    <div className="post-item-photo-index">
+                        <img className="post-item-photo-23-half" src={this.props.photos[0]} />
+                        <img className="post-item-photo-23-half" src={this.props.photos[1]} />
+                        <img className="post-item-photo-13" src={this.props.photos[2]} />
+                        <img className="post-item-photo-13" src={this.props.photos[3]} />
+                        <img className="post-item-photo-13" src={this.props.photos[4]} />
+                    </div>
+                )
+            default:
+                return (
+                    <div className="post-item-photo-index">
+                        <img className="post-item-photo-23-half" src={this.props.photos[0]} />
+                        <img className="post-item-photo-23-half" src={this.props.photos[1]} />
+                        <img className="post-item-photo-13" src={this.props.photos[2]} />
+                        <img className="post-item-photo-13" src={this.props.photos[3]} />
+                        <div className="post-item-index-more">
+                            <img className="post-item-photo-13" src={this.props.photos[4]} />
+                            <div className="post-item-index-more-overlay">+{numPhotos - 4}</div>
+                        </div>
+
+                    </div>
+                )
+        }
     }
 
     render() {
         if (this.props.photos) {
             return (
-                <Grid container spacing={3} direction="column" class="post-item-photo-index">
-                    {this.props.photos.map(photo => <img className="post-item-photo-item" src={photo} />)}
-                </Grid>
+                <>
+                    {this.renderIndex()}
+                </>
             )
         } else {
             return null;
