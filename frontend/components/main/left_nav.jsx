b42ff24a4a9c4c4834ea8efbc6b2091e3bd0d9e2
Eric Liang <eliang58@gmail.com>
2019-06-23 21:43:51 -0400

    add news feed left nav

--- frontend/components/main/left_nav.jsx
+++ frontend/components/main/left_nav.jsx
@@ -1,8 +1,42 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { NavLink, withRouter } from 'react-router-dom';
+import {
+    FaRegNewspaper,
+    FaGithub,
+    FaLinkedin
+} from 'react-icons/fa';
 
-// const LeftNav = props => {
-//     return (
+const LeftNav = props => {
+    return (
+        <nav className="left-nav">
+            <NavLink className="left-nav-link" to={`/users/${props.currentUser}`} >
+                <img className="left-nav-avatar-icon" src={props.currentPhoto} />
+                {props.currentName}
+            </NavLink>
+            <NavLink className="left-nav-link" to="/" >
+                <FaRegNewspaper className="left-nav-icon" />
+                News Feed
+            </NavLink>
+            <a className="left-nav-link" href="https://github.com/clericl/felix">
+                <FaGithub className="left-nav-icon" />
+                GitHub
+            </a>
+            <a className="left-nav-link" href="https://www.linkedin.com/in/eliang58/">
+                <FaLinkedin className="left-nav-icon" />
+                LinkedIn
+            </a>
+            <p className="left-nav-footer">© 2019 Eric Liang. Inspired by Facebook.</p>
+        </nav>
+    )
+}
 
-//     )
-// }
+const msp = (state, ownProps) => {
+    return {
+        currentUser: state.session.currentUser,
+        currentPhoto: state.entities.users[state.session.currentUser].defaultImgUrl,
+        currentName: state.entities.users[state.session.currentUser].firstName + " " + state.entities.users[state.session.currentUser].lastName,
+    };
+}
+
+export default withRouter(connect(msp, null)(LeftNav));
--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LeftNav from './left_nav';
 import NewsFeed from './news_feed';
 import { NewsFeedPostModal } from './posts/create_post_modal';
 
@@ -6,6 +7,7 @@ class Main extends React.Component {
     render() {
         return (
             <main className="main-content">
+                <LeftNav />
                 <div className="main-center-column">
                     <NewsFeedPostModal />
                     <NewsFeed />
